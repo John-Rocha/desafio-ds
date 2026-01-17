abf7204070eb014a855323531412e50e9b777730
Johnathan Rocha <johnathanrocha@gmail.com>
2026-01-17 15:05:52 -0300

    Initial commit

---
+++ src/main/java/com/desafio/desafio_ds/DesafioDsApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.desafio_ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDsApplication.class, args);
+	}
+
+}
